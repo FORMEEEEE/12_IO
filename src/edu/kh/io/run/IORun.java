f40491662420ea2a9612d3138dc174edc8e630a3
RYU <mrryu555@gmail.com>
2022-12-30 15:16:58 +0900

    first

---
+++ src/edu/kh/io/run/IORun.java
@@ -0,0 +1,16 @@
+package edu.kh.io.run;
+
+import edu.kh.io.model.service.IOService;
+
+public class IORun {
+
+	public static void main(String[] args) {
+		
+		IOService io = new  IOService();
+		io.input2();
+		
+		
+	}
+	
+	
+}
